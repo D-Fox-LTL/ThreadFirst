95968d869145c22e42f3304772866894a337f4e6
D-Fox-LTL <99818044+D-Fox-LTL@users.noreply.github.com>
2023-05-31 09:55:31 +0200

    Add files via upload

---
+++ ThreadTester/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace ThreadTester
+{
+    public partial class Form1 : Form
+    {
+        // New timer
+        clsTimer mobjTimer;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //-----------------------------------------------------------
+        // After button click timer starts
+        //-----------------------------------------------------------
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Start timeru
+            mobjTimer = new clsTimer(100);
+
+
+                // Using += because event cannot be equals to sth therefor it
+                // will delete eveerything in que and any other thing won't be processed
+            mobjTimer.Tick+=clsTimer_Tick;
+            mobjTimer.mobjForm = this;
+            mobjTimer.Start();
+        }
+
+
+        //-----------------------------------------------------------
+        // Tick of timer
+        //-----------------------------------------------------------
+        void clsTimer_Tick()
+        {
+            textBox1.Text = "nn";
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            mobjTimer.Stop();
+        }
+    }
+}
---
+++ ThreadTester/clsTimer.cs
@@ -0,0 +1,82 @@
+﻿using System;using System.Collections.Generic;using System.Linq;using System.Runtime.CompilerServices;
+using System.Text;using System.Threading;
+using System.Threading.Tasks;using System.Windows.Forms;
+
+namespace ThreadTester{    internal class clsTimer    {
+        // Interval in ms
+        int mintIntervalIn_ms;
+
+
+        // thread for timer timing
+        Thread mobjThreadOne;
+
+
+        // Thread stop
+        bool mblStop;
+
+
+        // definition of event form
+        public delegate void dlgTick();
+        // Declaration of event
+        public event dlgTick Tick;
+
+        // Form where my obj exists
+        public Form mobjForm;
+
+
+        //\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//
+        // Interval of timer tick
+        //\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//
+        public int Interval
+        {
+            get { return mintIntervalIn_ms; }
+            set { mintIntervalIn_ms = value;  }
+        }
+
+
+        //-----------------------------------------------------------
+        // Timer constructor
+        //-----------------------------------------------------------
+        public clsTimer(int intInterval)        {            mintIntervalIn_ms = Math.Abs(intInterval);            mobjForm = null;        }
+
+
+        //-----------------------------------------------------------
+        // Timer start
+        //-----------------------------------------------------------
+        public bool Start()        {            // Creation of thread   (Not in constructor because after stop it wouldn't start up)
+            //              !Don't start it before stoping the thread!            mobjThreadOne = new Thread(MyRoutine);
+
+            //
+            mblStop = false;
+            mobjThreadOne.Start();            return true;        }
+
+        //-----------------------------------------------------------
+        // Thread timeru
+        //-----------------------------------------------------------
+
+        private void MyRoutine()
+        {
+            do
+            {
+                // call of events in form thread
+                if(mobjForm!=null)
+                    mobjForm.Invoke(Tick);
+                // Tick();
+
+                // will make thread asleep Zzzzzz
+                //Thread.Sleep(mintIntervalIn_ms);
+                do 
+                { 
+                    DateTime.Ticks   //TODO: microsecs
+                } while ();
+                
+            } while (mblStop == false);
+        }
+
+        //-----------------------------------------------------------
+        // Timer stop
+        //-----------------------------------------------------------
+        public bool Stop()        {
+            mobjForm = null;
+            // Stop thread
+            mblStop = true;            return true;        }    }}
